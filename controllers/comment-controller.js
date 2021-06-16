b41b6c82dfaa61e798f021ddbb22881302404455
nmisustin <nmisustin@gmail.com>
2021-06-16 13:53:00 -0400

    lesson 3

--- controllers/comment-controller.js
+++ controllers/comment-controller.js
@@ -20,6 +20,21 @@ const commentController = {
             })
             .catch(err => res.json(err));
     },
+    addReply({params, body}, res){
+        Comment.findByIdAndUpdate(
+            {_id: params.commentId},
+            {$push: {replies: body}},
+            {new: true}
+        )
+            .then(dbPizzaData => {
+                if(!dbPizzaData){
+                    res.status(404).json({message: 'No pizza with this id was found.'});
+                    return;
+                }
+                res.json(dbPizzaData)
+            })
+            .catch(err => res.json(err));
+    },
     removeComment({ params }, res) {
         Comment.findOneAndDelete({ _id: params.commentId })
             .then(deleteComment => {
@@ -39,6 +54,15 @@ const commentController = {
                 }
                 res.json(dbPizzaData)
             })
+            .catch(err => res.json(err));
+    },
+    removeReply({params}, res) {
+        Comment.findOneAndUpdate(
+            {_id: params.commentId},
+            {$pull: {replies: {replyId: params.replyId}}},
+            {new: true }
+        )
+            .then(dbPizzaData => res.json(dbPizzaData))
             .catch(err => res.json(err));
     }
 };
